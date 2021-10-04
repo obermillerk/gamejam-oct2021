a5da542984f987188dac095c4907089645c0d15a
Karl Obermiller <karl@obermiller.dev>
2021-10-04 02:50:57 -0400

    ForceField unlocked!

--- Assets/Scripts/LockUsable.cs
+++ Assets/Scripts/LockUsable.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class LockUsable : Usable {
 
     [SerializeField] private KeyCollectible key;
+
+    [SerializeField] private GameObject forceField;
     
     public override bool UseWith(Collectible other) {
         if (other.Equals(key)) {
             Debug.Log("Used the right key!");
             Destroy(gameObject);
+
+            var emission = forceField.GetComponent<ParticleSystem>().emission;
+            emission.enabled = false;
+            Destroy(forceField, 1f);
             return true;
         }
 
