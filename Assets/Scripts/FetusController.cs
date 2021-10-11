de7eebd7769156638636ea39d82ecfae7620880a
Karl Obermiller <karl@obermiller.dev>
2021-10-10 22:46:45 -0400

    Fix int overflow maybe? (Fetus damage calculation)

---
+++ Assets/Scripts/FetusController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FetusController : MonoBehaviour {
+    private void Start() {
+        var rigidbody2D = GetComponent<Rigidbody2D>();
+
+        rigidbody2D.angularDrag = 0;
+        rigidbody2D.angularVelocity = 30;
+    }
+}
